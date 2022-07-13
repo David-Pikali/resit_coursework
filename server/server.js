ee2ae94e81fb45aed16855b7ad5a7263b319c0c0
David <david@Davids-MacBook-Pro.local>
2022-07-13 21:43:12 +0200

    removing the user login data from the file

--- server/server.js
+++ server/server.js
@@ -3,8 +3,6 @@ import app from './express'
 import mongoose from 'mongoose'
 
 // Connection URL
-//const uri = "mongodb+srv://David:12345myPASSWORD@ewscoursework.xgrkf.mongodb.net/?retryWrites=true&w=majority"
-
 mongoose.Promise = global.Promise
 mongoose.connect(config.mongoUri, { dbName: "users" })
 mongoose.connection.on('error', err => {
